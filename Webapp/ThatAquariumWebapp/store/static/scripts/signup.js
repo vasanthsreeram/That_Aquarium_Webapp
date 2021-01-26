5face37fa9866cae037885945726b4b57cc5515a
InFInItyKiLL33 <2poh.junkang@gmail.com>
2021-01-26 23:11:59 +0800

    change header and need fix bugs pls

--- Webapp/ThatAquariumWebapp/store/static/scripts/signup.js
+++ Webapp/ThatAquariumWebapp/store/static/scripts/signup.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
         $(".error-5").html($(".error-5").contents()[1]["innerText"].split(/\n/)[1])
     };
     var form_fields = document.getElementsByTagName('input')
-        form_fields[2].placeholder='Full Name';
+        form_fields[1].placeholder='Full Name';
+        form_fields[1].className='email';
+        form_fields[2].placeholder='Email';
+        form_fields[2].type='email';
         form_fields[2].className='email';
-        form_fields[3].placeholder='Email';
-        form_fields[3].type='email';
-        form_fields[3].className='email';
-        form_fields[4].placeholder='Password';
+        form_fields[3].placeholder='Password';
+        form_fields[3].className='password';
+        form_fields[4].placeholder='Confirm Password';
         form_fields[4].className='password';
-        form_fields[5].placeholder='Confirm Password';
-        form_fields[5].className='password';
 });
