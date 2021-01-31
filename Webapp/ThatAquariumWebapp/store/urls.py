59fa1fde3c9339d9f11cb1e310481f4d52c6e50f
vasanth <s.vasanthrojin@gmail.com>
2021-01-31 14:26:44 +0800

    finished the forget password boiiiiiiiii now to do the activate email which is surprisingly harder than I thought

--- Webapp/ThatAquariumWebapp/store/urls.py
+++ Webapp/ThatAquariumWebapp/store/urls.py
@@ -29,12 +29,10 @@ urlpatterns = [
     path('update_address/',views.updateAddress,name='update_address'),
 
 
-    path('reset_password/',auth_views.PasswordResetView.as_view(
-        template_name='home_page/forget_password.html'
-    ),name='reset_password'),
-    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
-    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name='reset_password_complete'),
+    path('reset_password/',auth_views.PasswordResetView.as_view(template_name='home_page/forget_password.html'),name='reset_password'),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(template_name='home_page/password_reset_sent.html'),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='home_page/password_reset_form.html'),name="password_reset_confirm"),
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(template_name='home_page/password_reset_done.html'),name='password_reset_complete'),
 
 
 
